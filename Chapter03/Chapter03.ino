241b507ac5531d5169b377dfd01306990746825a
Mads Peter Emborg <madspeteremborg@live.dk>
2015-12-26 17:31:53 +0100

    Setup comment

--- Chapter03/Chapter03.ino
+++ Chapter03/Chapter03.ino
@@ -1,6 +1,6 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600); // Open serial port and sets data rate
   Serial.println("Hello World!");
 }
 
